78ef82cf356be911994f80156c71636fdaccb090
Aditya Raj <adistrim.dev@gmail.com>
2024-01-20 20:03:04 +0530

    header updated

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC = () => {
             <div className='ml-[10rem] mr-[10rem] flex justify-between items-center'>
                 <div className="flex items-center">
                     <img
-                        src="aic-logo.png"
+                        src="aic-logo-text.png"
                         alt="Logo"
                         className="h-14 m-1 my-0.5"
                     />
@@ -24,13 +24,19 @@ const Header: React.FC = () => {
 
                     {/* Dropdown */}
                     <div className="relative group">
-                        <a href="#" className={`cursor-pointer text-black ${pathname === '/programs' ? 'text-[#f5b051]' : 'hover:text-[#f5b051]'}`}>
+                        <p className={`cursor-pointer text-black ${pathname === '/programs' ? 'text-[#f5b051]' : 'hover:text-[#f5b051]'}`}>
                             Program
-                        </a>
-                        <ul className="absolute hidden bg-gray-700 text-black p-2 space-y-2 group-hover:block">
-                            <li><a href="#">Option 1</a></li>
-                            <li><a href="#">Option 2</a></li>
-                            <li><a href="#">Option 3</a></li>
+                        </p>
+                        <ul className="absolute hidden bg-white text-black p-3 space-y-2 group-hover:block shadow-md rounded-md transition-all duration-300">
+                            <li>
+                                <a href="#" className="block px-2 py-2 hover:text-[#f5b051] whitespace-nowrap">Incubation Program</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block px-2 py-2 hover:text-[#f5b051] whitespace-nowrap">Accelerator Program</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block px-2 py-2 hover:text-[#f5b051] whitespace-nowrap">SAIP</a>
+                            </li>
                         </ul>
                     </div>
 
@@ -38,10 +44,16 @@ const Header: React.FC = () => {
                         <a href="#" className={`cursor-pointer text-black ${pathname === '/stakeholders' ? 'text-[#f5b051]' : 'hover:text-[#f5b051]'}`}>
                             Stakeholders
                         </a>
-                        <ul className="absolute hidden bg-gray-700 text-black p-2 space-y-2 group-hover:block">
-                            <li><a href="#">Option 1</a></li>
-                            <li><a href="#">Option 2</a></li>
-                            <li><a href="#">Option 3</a></li>
+                        <ul className="absolute hidden bg-white text-black p-3 space-y-2 group-hover:block shadow-md rounded-md transition-all duration-300">
+                            <li>
+                                <a href="#" className="block px-2 py-1 hover:text-[#f5b051] whitespace-nowrap">Mentors</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block px-2 py-1 hover:text-[#f5b051] whitespace-nowrap">Team</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block px-2 py-1 hover:text-[#f5b051] whitespace-nowrap">Partners</a>
+                            </li>
                         </ul>
                     </div>
                 </nav>
