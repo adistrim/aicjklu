4b9253f4d1bed954badbf7ffe0ec5c9a8b207f99
Aditya Raj <adistrim.dev@gmail.com>
2024-01-21 11:50:27 +0530

    partner component is responsive now

--- src/app/components/partners.tsx
+++ src/app/components/partners.tsx
@@ -4,60 +4,66 @@ import NextLink from "next/link";
 
 const Partners: React.FC = () => {
     return (
-        <div className="ml-[10rem] mr-[10rem] mt-[34rem] flex flex-col items-center">
-            <div className="flex space-x-16 justify-center">
-                <div className="flex-none">
+        <div className="mx-[1rem] md:mx-[10rem] md:mt-[20rem] mt-[12rem] lg:mt-[34rem] flex flex-col items-center">
+            <div className="flex flex-wrap md:flex-nowrap space-y-4 space-x-4 md:space-y-0 md:space-x-16 justify-center">
+                <div className="flex-none relative">
                     <Image
                         src={"/niti-aayog.png"}
                         alt={""}
-                        width={125}
-                        height={125}
+                        width={100}
+                        height={100}
+                        className="w-20 h-20 lg:w-32 lg:h-32"
                     />
                 </div>
-                <div className="flex-none">
+                <div className="flex-none relative">
                     <Image
                         src={"/aim.png"}
                         alt={""}
-                        width={125}
-                        height={125}
+                        width={100}
+                        height={100}
+                        className="w-20 h-20 lg:w-32 lg:h-32"
                     />
                 </div>
-                <div className="flex-none">
+                <div className="flex-none relative">
                     <Image
                         src={"/msme.png"}
                         alt={""}
-                        width={125}
-                        height={125}
+                        width={100}
+                        height={100}
+                        className="w-20 h-20 lg:w-32 lg:h-32"
                     />
                 </div>
-                <div className="flex-none">
+                <div className="flex-none relative">
                     <Image
                         src={"/DoITM.png"}
                         alt={""}
-                        width={125}
-                        height={125}
+                        width={100}
+                        height={100}
+                        className="w-20 h-20 lg:w-32 lg:h-32"
                     />
                 </div>
-                <div className="flex-none">
+                <div className="flex-none relative">
                     <Image
                         src={"/startup-himachal.png"}
                         alt={""}
-                        width={125}
-                        height={125}
+                        width={100}
+                        height={100}
+                        className="w-20 h-20 lg:w-32 lg:h-32"
                     />
                 </div>
-                <div className="flex-none">
+                <div className="flex-none relative">
                     <Image
                         src={"/seed-fund.png"}
                         alt={""}
-                        width={125}
-                        height={125}
+                        width={100}
+                        height={100}
+                        className="w-20 h-20 lg:w-32 lg:h-32"
                     />
                 </div>
             </div>
             <div className="self-end mr-[1rem]">
                 <NextLink href="/partners" passHref>
-                    <p className="relative text-[#f5b051] flex items-center px-4 py-2 rounded-md overflow-hidden group hover:text-white">
+                    <p className="relative text-sm lg:text-normal text-[#f5b051] flex items-center px-4 py-2 rounded-md overflow-hidden group hover:text-white">
                         <span className="absolute inset-0 bg-[#f5b051] w-0 transition-all duration-300 group-hover:w-full"></span>
                         <span className="z-10">See More &#8594;</span>
                     </p>
