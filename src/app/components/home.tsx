100f6de2a9ee69e8b5fa08f72ef861894e1d12c4
Aditya Raj <adistrim.dev@gmail.com>
2024-01-21 11:22:13 +0530

    homeArea is responsive now

--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const HomeArea: React.FC = () => {
     return (
-        <div className="bg-white">
+        <div className="bg-white relative">
             <Image
                 className="absolute top-0 left-0 z-[-1]"
                 src={"/banner.png"}
@@ -13,14 +13,14 @@ const HomeArea: React.FC = () => {
             >
             </Image>
 
-            <div className="absolute ml-[10rem] mt-[10rem]">
-                <h1 className="text-[4rem] font-bold text-black">
+            <div className="absolute ml-[3rem] mt-[4rem] md:ml-[5rem] md:mt-[5rem] lg:ml-[10rem] lg:mt-[10rem]">
+                <h1 className="text-[1rem] md:text-[3rem] lg:text-[4rem] font-bold text-black">
                     Driving
                 </h1>
-                <h1 className="text-[4rem] font-bold text-black">
+                <h1 className="text-[1rem] md:text-[3rem] lg:text-[4rem] font-bold text-black">
                     Innovation for
                 </h1>
-                <h1 className="text-[4rem] font-bold text-black">
+                <h1 className="text-[1rem] md:text-[3rem] lg:text-[4rem] font-bold text-black">
                     a Better Future
                 </h1>
             </div>
