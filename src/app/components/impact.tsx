acb129a7b53f99bf31ea75130b8336d8ce612a5c
Aditya Raj <adistrim.dev@gmail.com>
2024-01-21 11:56:08 +0530

    impact component is responsive now

--- src/app/components/impact.tsx
+++ src/app/components/impact.tsx
@@ -2,33 +2,33 @@ import React from 'react';
 
 const Impact: React.FC = () => {
     return (
-        <div className="py-12 mt-[2rem] mr-[10rem] border-t border-[#f5b051] ml-[10rem] bg-white">
+        <div className="py-12 mt-[2rem] mx-[1rem] md:mx-[5rem] lg:mx-[10rem] border-t border-[#f5b051] bg-white">
             <div className="container mx-auto">
-                <h2 className="text-4xl font-bold mb-8 text-center">Our Impact</h2>
+                <h2 className="text-2xl lg:text-4xl font-bold mb-8 text-center">Our Impact</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
                     <div className="p-6 bg-gray-100 rounded-md shadow-md">
-                        <p className="text-3xl font-bold text-[#f5b051] mb-2">200+</p>
-                        <p className="text-gray-700 text-lg">Startups</p>
+                        <p className="text-xl lg:text-3xl font-bold text-[#f5b051] mb-2">200+</p>
+                        <p className="text-gray-700 text-normal lg:text-lg">Startups</p>
                     </div>
                     <div className="p-6 bg-gray-100 rounded-md shadow-md">
-                        <p className="text-3xl font-bold text-[#f5b051] mb-2">₹3.2 Cr</p>
-                        <p className="text-gray-700 text-lg">Raised by incubated startups</p>
+                        <p className="text-xl lg:text-3xl font-bold text-[#f5b051] mb-2">₹3.2 Cr</p>
+                        <p className="text-gray-700 text-normal lg:text-lg">Raised by incubated startups</p>
                     </div>
                     <div className="p-6 bg-gray-100 rounded-md shadow-md">
-                        <p className="text-3xl font-bold text-[#f5b051] mb-2">45+</p>
-                        <p className="text-gray-700 text-lg">Mentors</p>
+                        <p className="text-xl lg:text-3xl font-bold text-[#f5b051] mb-2">45+</p>
+                        <p className="text-gray-700 text-normal lg:text-lg">Mentors</p>
                     </div>
                     <div className="p-6 bg-gray-100 rounded-md shadow-md">
-                        <p className="text-3xl font-bold text-[#f5b051] mb-2">600+</p>
-                        <p className="text-gray-700 text-lg">Events</p>
+                        <p className="text-xl lg:text-3xl font-bold text-[#f5b051] mb-2">600+</p>
+                        <p className="text-gray-700 text-normal lg:text-lg">Events</p>
                     </div>
                     <div className="p-6 bg-gray-100 rounded-md shadow-md">
-                        <p className="text-3xl font-bold text-[#f5b051] mb-2">100+</p>
-                        <p className="text-gray-700 text-lg">Exclusive programs</p>
+                        <p className="text-xl lg:text-3xl font-bold text-[#f5b051] mb-2">100+</p>
+                        <p className="text-gray-700 text-normal lg:text-lg">Exclusive programs</p>
                     </div>
                     <div className="p-6 bg-gray-100 rounded-md shadow-md">
-                        <p className="text-3xl font-bold text-[#f5b051] mb-2">35+</p>
-                        <p className="text-gray-700 text-lg">Partners</p>
+                        <p className="text-xl lg:text-3xl font-bold text-[#f5b051] mb-2">35+</p>
+                        <p className="text-gray-700 text-normal lg:text-lg">Partners</p>
                     </div>
                 </div>
             </div>
