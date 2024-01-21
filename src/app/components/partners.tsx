84548ebd593823824e1a7ed366aee7fc588ab506
Aditya Raj <adistrim.dev@gmail.com>
2024-01-21 08:51:37 +0530

    added see more button in partners component

--- src/app/components/partners.tsx
+++ src/app/components/partners.tsx
@@ -1,56 +1,67 @@
 import React from "react";
 import Image from "next/image";
+import NextLink from "next/link";
 
 const Partners: React.FC = () => {
     return (
-        <div className="ml-[10rem] mr-[10rem] mt-[34rem] flex space-x-16 justify-center">
-            <div className="flex-none">
-                <Image
-                    src={"/niti-aayog.png"}
-                    alt={""}
-                    width={125}
-                    height={125}
-                />
+        <div className="ml-[10rem] mr-[10rem] mt-[34rem] flex flex-col items-center">
+            <div className="flex space-x-16 justify-center">
+                <div className="flex-none">
+                    <Image
+                        src={"/niti-aayog.png"}
+                        alt={""}
+                        width={125}
+                        height={125}
+                    />
+                </div>
+                <div className="flex-none">
+                    <Image
+                        src={"/aim.png"}
+                        alt={""}
+                        width={125}
+                        height={125}
+                    />
+                </div>
+                <div className="flex-none">
+                    <Image
+                        src={"/msme.png"}
+                        alt={""}
+                        width={125}
+                        height={125}
+                    />
+                </div>
+                <div className="flex-none">
+                    <Image
+                        src={"/DoITM.png"}
+                        alt={""}
+                        width={125}
+                        height={125}
+                    />
+                </div>
+                <div className="flex-none">
+                    <Image
+                        src={"/startup-himachal.png"}
+                        alt={""}
+                        width={125}
+                        height={125}
+                    />
+                </div>
+                <div className="flex-none">
+                    <Image
+                        src={"/seed-fund.png"}
+                        alt={""}
+                        width={125}
+                        height={125}
+                    />
+                </div>
             </div>
-            <div className="flex-none">
-                <Image
-                    src={"/aim.png"}
-                    alt={""}
-                    width={125}
-                    height={125}
-                />
-            </div>
-            <div className="flex-none">
-                <Image
-                    src={"/msme.png"}
-                    alt={""}
-                    width={125}
-                    height={125}
-                />
-            </div>
-            <div className="flex-none">
-                <Image
-                    src={"/DoITM.png"}
-                    alt={""}
-                    width={125}
-                    height={125}
-                />
-            </div>
-            <div className="flex-none">
-                <Image
-                    src={"/startup-himachal.png"}
-                    alt={""}
-                    width={125}
-                    height={125}
-                />
-            </div>
-            <div className="flex-none">
-                <Image
-                    src={"/seed-fund.png"}
-                    alt={""}
-                    width={125}
-                    height={125}
-                />
+            <div className="self-end mr-[1rem]">
+                <NextLink href="/partners" passHref>
+                    <p className="relative text-[#f5b051] flex items-center px-4 py-2 rounded-md overflow-hidden group hover:text-white">
+                        <span className="absolute inset-0 bg-[#f5b051] w-0 transition-all duration-300 group-hover:w-full"></span>
+                        <span className="z-10">See More &#8594;</span>
+                    </p>
+                </NextLink>
             </div>
         </div>
     );
